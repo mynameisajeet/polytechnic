0aa819c7c1fb57e0c1830d91ca57bbf693941d89
mynameisajeet <71747902+mynameisajeet@users.noreply.github.com>
2024-10-25 14:15:07 +0530

    Add files via upload

---
+++ course.js
@@ -0,0 +1,28 @@
+{
+  "Courses": [
+    {
+      "Branch": "Civil Engineering",
+      "Capacity": 75
+    },
+    {
+      "Branch": "Electrical Engineering",
+      "Capacity": 75
+    },
+    {
+      "Branch": "Mechanical Engineering",
+      "Capacity": 75
+    },
+    {
+      "Branch": "Civil Engineering (Lateral Entry)",
+      "Capacity": 7
+    },
+    {
+      "Branch": "Electrical Engineering (Lateral Entry)",
+      "Capacity": 7
+    },
+    {
+      "Branch": "Mechanical Engineering (Lateral Entry)",
+      "Capacity": 7
+    }
+  ]
+}
